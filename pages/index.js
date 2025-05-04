763f7164541a792cba4d26b0c38097a60da03009
bulcao98 <gabrielfilipe.bz@gmail.com>
2025-05-04 10:39:42 -0300

    Adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Next.js App With Amends'</h1>
+      <p>This is a simple example of a Next.js application.</p>
+    </div>
+  );
+}
+export default Home;
